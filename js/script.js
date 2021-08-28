386cdfe218d98d81e2fb5f29efbffaaf914c5c91
hoangnhutvu <vu23071997@gmail.com>
2021-08-28 17:39:08 +0700

    Fix roadmap anh rate of token

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 /* PRELOADER */
-$(window).on('load', function() {
-    setTimeout(() => {
-        $(".preloader").delay(500).fadeOut("slow");
-    }, 500);
-})
+$(window).on("load", function () {
+  setTimeout(() => {
+    $(".preloader").delay(500).fadeOut("slow");
+  }, 500);
+});
 
 /* SCROLL TO TOP */
 function handleToggleScrollToTop() {
-    if (window.scrollY > 200) {
-        $('#toTop').fadeIn(300);
-    } else {
-        $('#toTop').fadeOut(300);
-    }
+  if (window.scrollY > 200) {
+    $("#toTop").fadeIn(300);
+  } else {
+    $("#toTop").fadeOut(300);
+  }
 }
 handleToggleScrollToTop();
 window.addEventListener("scroll", handleToggleScrollToTop);
@@ -19,21 +19,21 @@ window.addEventListener("scroll", handleToggleScrollToTop);
 /* SLIDE */
 
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-        shadow: true,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+    shadow: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
 swiper.slideTo(2);
 // var swiper = new Swiper(".mySwiper", {
@@ -80,81 +80,111 @@ swiper.slideTo(2);
 //     }
 // });
 
-
 /* DRAW CHART */
-const canvas_border_color = '#7397c9';
-const pie_color = '#3c5a85';
-const pie_hover_color = '#ff3549';
+const canvas_border_color = "#7397c9";
+const pie_color = "#3c5a85";
+const pie_hover_color = "#ff3549";
 const data_set = {
-    labels: ['Ecosystem', 'Reward', 'Dev Team', 'Founder Team', 'Advisors', 'Marketing', 'Private sale', 'Public IDO'],
-    titles: '',
-    datasets: [{
-        label: "949",
-        lineTension: 0,
-        backgroundColor: ['#6FAFD5', pie_color, pie_color, pie_color, pie_color, pie_color, pie_color, pie_color],
-        hoverBackgroundColor: [pie_hover_color, pie_hover_color, pie_hover_color, pie_hover_color, pie_hover_color, pie_hover_color, pie_hover_color, pie_hover_color],
-        borderColor: canvas_border_color,
-        borderWidth: 1,
-        hoverBorderColor: canvas_border_color,
-        data: [40, 15, 10, 10, 4, 10, 6, 5],
-        animationDuration: 400,
-    }]
+  labels: [
+    "Ecosystem",
+    "Reward",
+    "Dev Team",
+    "Founder Team",
+    "Advisors",
+    "Marketing",
+    "Private sale",
+    "Public IDO",
+  ],
+  titles: "",
+  datasets: [
+    {
+      label: "949",
+      lineTension: 0,
+      backgroundColor: [
+        "#6FAFD5",
+        pie_color,
+        pie_color,
+        pie_color,
+        pie_color,
+        pie_color,
+        pie_color,
+        pie_color,
+      ],
+      hoverBackgroundColor: [
+        pie_hover_color,
+        pie_hover_color,
+        pie_hover_color,
+        pie_hover_color,
+        pie_hover_color,
+        pie_hover_color,
+        pie_hover_color,
+        pie_hover_color,
+      ],
+      borderColor: canvas_border_color,
+      borderWidth: 1,
+      hoverBorderColor: canvas_border_color,
+      data: [30, 35, 10, 6, 3, 10, 4, 2],
+      animationDuration: 400,
+    },
+  ],
 };
 const options_set = {
-    cutout: '40%',
-    plugins: {
-        legend: {
-            display: false
-        },
-        cutoutPercentage: 0,
-        maintainAspectRatio: false,
-        tooltip: {
-            callbacks: {
-                title: function(tooltipItem) {
-                    return [tooltipItem[0]['label']];
-                },
-                label: function(tooltipItem, data) {
-                    return tooltipItem['raw'] + ' %';
-                }
-            },
-            backgroundColor: 'transparent',
-            titleFontSize: 11,
-            bodyFontColor: '#fff',
-            bodyFontSize: 14,
-            bodyFontStyle: 'bold',
-            bodySpacing: 0,
-            yPadding: 0,
-            xPadding: 0,
-            yAlign: 'center',
-            xAlign: 'center',
-            footerMarginTop: 5,
-            displayColors: false
-        },
+  cutout: "40%",
+  plugins: {
+    legend: {
+      display: false,
     },
-    onHover: function(e, i) {
-        if (i.length) {
-            var _cur_idx = i[0].index + 1;
-            $('#ic-chart-legends li').removeClass('active');
-            $('#ic-chart-legends li:nth-child(' + _cur_idx + ')').addClass('active');
-        } else {
-            $('#ic-chart-legends li').removeClass('active');
-        }
+    cutoutPercentage: 0,
+    maintainAspectRatio: false,
+    tooltip: {
+      callbacks: {
+        title: function (tooltipItem) {
+          return [tooltipItem[0]["label"]];
+        },
+        label: function (tooltipItem, data) {
+          return tooltipItem["raw"] + " %";
+        },
+      },
+      backgroundColor: "transparent",
+      titleFontSize: 11,
+      bodyFontColor: "#fff",
+      bodyFontSize: 14,
+      bodyFontStyle: "bold",
+      bodySpacing: 0,
+      yPadding: 0,
+      xPadding: 0,
+      yAlign: "center",
+      xAlign: "center",
+      footerMarginTop: 5,
+      displayColors: false,
+    },
+  },
+  onHover: function (e, i) {
+    if (i.length) {
+      var _cur_idx = i[0].index + 1;
+      $("#ic-chart-legends li").removeClass("active");
+      $("#ic-chart-legends li:nth-child(" + _cur_idx + ")").addClass("active");
+    } else {
+      $("#ic-chart-legends li").removeClass("active");
     }
+  },
 };
 
-$(function() {
-    new WOW().init();
+$(function () {
+  new WOW().init();
 
-    var ctx = document.getElementById('ICTokenChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: data_set,
-        options: options_set
-    });
-})
+  var ctx = document.getElementById("WARETokenChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "doughnut",
+    data: data_set,
+    options: options_set,
+  });
+});
 
 /* TOOLTIP */
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
